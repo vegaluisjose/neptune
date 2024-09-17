46a2f6fb13a4bfdb304f3ca4bb46285ba0623e98
Luis Vega <vegaluisjose@gmail.com>
2024-09-17 14:37:32 +0000

    added first two tests

--- README.md
+++ README.md
@@ -10,4 +10,30 @@ python3 -m pip install triton
 
 ## Examples
 
-* Vector add
+### Vector addition
+
+* Run
+```bash
+python3 vadd/test.py
+```
+
+* Output
+```bash
+tensor([1.3713, 1.3076, 0.4940,  ..., 0.6724, 1.2141, 0.9733], device='cuda:0')
+tensor([1.3713, 1.3076, 0.4940,  ..., 0.6724, 1.2141, 0.9733], device='cuda:0')
+The maximum difference between torch and triton is 0.0
+```
+
+### Vector addition with autotune
+
+* Run
+```bash
+TRITON_PRINT_AUTOTUNING=1 python3 vadd_autotune/test.py
+```
+
+* Output
+```
+Triton autotuning for function add_kernel finished after 0.48s; best config selected: BLOCK_SIZE: 128, num_warps: 4, num_ctas: 1, num_stages: 2, maxnreg: None;
+tensor([ 1.3120, -0.5226,  2.0826,  ..., -0.4852,  1.7825, -1.1772],
+       device='cuda:0')
+```
---
+++ vadd_autotune/test.py
@@ -0,0 +1,38 @@
+import torch
+import triton
+import triton.language as tl
+
+
+@triton.autotune(
+    configs=[
+        triton.Config({"BLOCK_SIZE": 128}, num_warps=4),
+        triton.Config({"BLOCK_SIZE": 256}, num_warps=8),
+    ],
+    key=["n_elements"],
+)
+@triton.jit
+def add_kernel(x_ptr, y_ptr, output_ptr, n_elements, BLOCK_SIZE: tl.constexpr):
+    pid = tl.program_id(axis=0)
+    block_start = pid * BLOCK_SIZE
+    offsets = block_start + tl.arange(0, BLOCK_SIZE)
+    mask = offsets < n_elements
+    x = tl.load(x_ptr + offsets, mask=mask)
+    y = tl.load(y_ptr + offsets, mask=mask)
+    output = x + y
+    tl.store(output_ptr + offsets, output, mask=mask)
+
+
+def vector_add(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+    output = torch.empty_like(x)
+    n_elements = output.numel()
+    grid = lambda meta: (triton.cdiv(n_elements, meta["BLOCK_SIZE"]),)
+    add_kernel[grid](x, y, output, n_elements)
+    return output
+
+
+if __name__ == "__main__":
+    # Example usage
+    x = torch.randn(1024, device="cuda")
+    y = torch.randn(1024, device="cuda")
+    output = vector_add(x, y)
+    print(output)
